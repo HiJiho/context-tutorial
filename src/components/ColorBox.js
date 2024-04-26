f8cd1f6a446dab3b66f757191c66df539e7f48f8
김지호 <jiho001108@gmail.com>
2024-04-26 23:27:55 +0900

    동적인 Context로 수정하기

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,18 +1,27 @@
-import ColorContext from "../contexts/color";
+import { ColorConsumer } from "../contexts/color";
 
 const ColorBox = () => {
 	return (
-		<ColorContext.Consumer>
-			{(value) => (
-				<div
-					style={{
-						width: "64px",
-						height: "64px",
-						background: value.color,
-					}}
-				/>
+		<ColorConsumer>
+			{(value) => ( // 객체 비구조화 할당으로 value 대신 { state } 사용해도 됨
+				<>
+					<div
+						style={{
+							width: "64px",
+							height: "64px",
+							background: value.state.color, // => { state } 사용 시, state.color로 바뀜
+						}}
+					/>
+					<div
+						style={{
+							width: "32px",
+							height: "32px",
+							background: value.state.subcolor,
+						}}
+					/>
+				</>
 			)}
-		</ColorContext.Consumer>
+		</ColorConsumer>
 	);
 };
 
--- src/contexts/color.js
+++ src/contexts/color.js
@@ -1,5 +1,31 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const ColorContext = createContext({ color: "black" });
+const ColorContext = createContext({ // Provider의 value의 객체의 형태와 일치시키기
+	state: { color: "black", subcolor: "red" },
+	actions: {
+		setColor: () => {},
+		setSubcolor: () => {},
+	},
+});
+
+const ColorProvider = ({ children }) => {
+	const [color, setColor] = useState("black");
+	const [subcolor, setSubcolor] = useState("red");
+
+	const value = { // state와 actions 객체를 따로따로 분리해 주면 Context 값 사용하기 편함
+		state: { color, subcolor },
+		actions: { setColor, setSubcolor },
+	};
+
+	return (
+		<ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+	);
+};
+
+// const ColorConsumer = ColorContext.Consumer와 같은 의미
+const { Consumer: ColorConsumer } = ColorContext;
+
+// ColorProvider와 ColorConsumer 내보내기
+export { ColorProvider, ColorConsumer };
 
 export default ColorContext;
