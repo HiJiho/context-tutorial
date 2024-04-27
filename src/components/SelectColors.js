6ee04512be6fa3e4c85cc8e51d08d6ee4a9c997c
김지호 <jiho001108@gmail.com>
2024-04-27 19:22:52 +0900

    동적 Context 적용

--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,22 +1,37 @@
+import { ColorConsumer } from "../contexts/color";
+
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 const SelectColors = () => {
 	return (
 		<div>
 			<h2>색상을 선택하세요.</h2>
-			<div style={{ display: "flex" }}>
-				{colors.map((color) => (
-					<div
-						key={color}
-						style={{
-							background: color,
-							width: "24px",
-							height: "24px",
-							cursor: "pointer",
-						}}
-					/>
-				))}
-			</div>
+			<p>
+				좌클릭: 큰 사각형 색상 선택 <br />
+				우클릭: 작은 사각형 색상 선택
+			</p>
+			<ColorConsumer>
+				{({ actions }) => (
+					<div style={{ display: "flex" }}>
+						{colors.map((color) => (
+							<div
+								key={color}
+								style={{
+									background: color,
+									width: "24px",
+									height: "24px",
+									cursor: "pointer",
+								}}
+								onClick={() => actions.setColor(color)}
+								onContextMenu={(e) => {
+									e.preventDefault(); // 마우스 오른쪽 버튼 클릭 시 메뉴가 뜨는 것 무시
+									actions.setSubcolor(color);
+								}}
+							/>
+						))}
+					</div>
+				)}
+			</ColorConsumer>
 			<hr />
 		</div>
 	);
