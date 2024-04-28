f63b7d137cfb2fc98ffc7600abe9a3a7439fe9ca
김지호 <jiho001108@gmail.com>
2024-04-28 10:57:53 +0900

    useContext Hook 적용

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,27 +1,25 @@
-import { ColorConsumer } from "../contexts/color";
+import { useContext } from "react";
+import ColorContext from "../contexts/color";
 
 const ColorBox = () => {
+	const { state } = useContext(ColorContext);
 	return (
-		<ColorConsumer>
-			{(value) => ( // 객체 비구조화 할당으로 value 대신 { state } 사용해도 됨
-				<>
-					<div
-						style={{
-							width: "64px",
-							height: "64px",
-							background: value.state.color, // => { state } 사용 시, state.color로 바뀜
-						}}
-					/>
-					<div
-						style={{
-							width: "32px",
-							height: "32px",
-							background: value.state.subcolor,
-						}}
-					/>
-				</>
-			)}
-		</ColorConsumer>
+		<>
+			<div
+				style={{
+					width: "64px",
+					height: "64px",
+					background: state.color, // => { state } 사용 시, state.color로 바뀜
+				}}
+			/>
+			<div
+				style={{
+					width: "32px",
+					height: "32px",
+					background: state.subcolor,
+				}}
+			/>
+		</>
 	);
 };
 
